8c325dab4b3a241c6d0620a34ccbe4aa6ff7cc27
neither jiang <neiternor@gmail.com>
2020-09-04 01:00:21 +0800

    add basic fragment navigation

--- app/src/main/java/com/example/coviewer/MainActivity.java
+++ app/src/main/java/com/example/coviewer/MainActivity.java
@@ -2,10 +2,14 @@ package com.example.coviewer;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.Navigation;
 
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +21,42 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.main_toolbar);
         setSupportActionBar(toolbar);
         toolbar.setTitle("");
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.browse);// set drawable icon
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                ((DrawerLayout) findViewById(R.id.main_drawerlayout)).openDrawer(Gravity.LEFT);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+    public boolean main_drawer_button_click(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                ((DrawerLayout) findViewById(R.id.main_drawerlayout)).openDrawer(Gravity.LEFT);
+                return true;
+            case R.id.main_drawer_item_one:
+                Navigation.findNavController(findViewById((R.id.main_fragment_host))).navigate(R.id.news_main_fragment);
+                ((DrawerLayout) findViewById(R.id.main_drawerlayout)).closeDrawers();
+                return true;
+            case R.id.main_drawer_item_two:
+                Navigation.findNavController(findViewById((R.id.main_fragment_host))).navigate(R.id.data_main_fragment);
+                ((DrawerLayout) findViewById(R.id.main_drawerlayout)).closeDrawers();
+                return true;
+            case R.id.main_drawer_item_three:
+                return true;
+            case R.id.main_drawer_item_four:
+                return true;
+            case R.id.main_drawer_item_five:
+                return true;
+            default:
+                return false;
+        }
     }
 }
